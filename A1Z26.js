58a561e9f5a5a13572561547ea5782d3d16f9257
Bernardo Brasil <bernbrasil@gmail.com>
2020-02-13 17:12:20 -0300

    Multiple numbers decryption

--- A1Z26.js
+++ A1Z26.js
@@ -1,14 +1,28 @@
 
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const rx = /\d{1,2}|\./g
 
-function decrypt(){
+function decrypt() {
   let codeNumber = document.getElementById("codeInput").value;
-  if (codeNumber < 1 || codeNumber > 26){
-    return alert("Invalid number.");
+  // if (codeNumber < 0 || codeNumber > 26) {
+  //   return alert("Invalid number.");
+  // }
+
+  let matches = codeNumber.match(rx)
+  let result = ""
+
+  for (let i = 0; i < matches.length; i++) {
+    if (matches[i] < 0 || matches[i] > 26) {
+      result += matches[i]
+    } else if (matches[i] == 0 || matches[i] == ".") {
+      result += alphabet[0];
+    } else {
+      result += alphabet[matches[i]]
+    }
   }
 
-  document.getElementById("result").innerText = alphabet[codeNumber-1].toUpperCase()
-  
+  document.getElementById("result").innerText = result
+
   //Removes the class "hidden" from the "resultArea" element
-  document.getElementsByClassName("resultArea")[0].classList.remove("hidden")
+  // document.getElementsByClassName("resultArea")[0].classList.remove("hidden")
 }
