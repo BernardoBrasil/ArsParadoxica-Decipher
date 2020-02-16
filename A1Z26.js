f20bc39cffaf6edd4f3b9422f55c8f3465c16c4a
Bernardo Brasil <bernbrasil@gmail.com>
2020-02-16 17:20:27 -0300

    Name updates

--- A1Z26.js
+++ A1Z26.js
@@ -1,28 +1,22 @@
+const alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const rxNumber = /\d{1,2}|\./g
+const rxWord = /\w|\s/g
 
-const alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const rx = /\d{1,2}|\./g
+function decryptA1Z26() {
+  let numberIput = document.getElementById('numberInput').value;
 
-function decrypt() {
-  let codeNumber = document.getElementById("codeInput").value;
-  // if (codeNumber < 0 || codeNumber > 26) {
-  //   return alert("Invalid number.");
-  // }
+  let numberMatch = numberIput.match(rxNumber)
+  let A1Z26Result = '' 
 
-  let matches = codeNumber.match(rx)
-  let result = ""
-
-  for (let i = 0; i < matches.length; i++) {
-    if (matches[i] < 0 || matches[i] > 26) {
-      result += matches[i]
-    } else if (matches[i] == 0 || matches[i] == ".") {
-      result += alphabet[0];
+  for (let i = 0; i < numberMatch.length; i++) {
+    if (numberMatch[i] < 0 || numberMatch[i] > 26) {
+      A1Z26Result += numberMatch[i]
+    } else if (numberMatch[i] == '/' || numberMatch[i] == '.') {
+      A1Z26Result += alphabet[0];
     } else {
-      result += alphabet[matches[i]]
+      A1Z26Result += alphabet[numberMatch[i]]
     }
   }
 
-  document.getElementById("result").innerText = result
-
-  //Removes the class "hidden" from the "resultArea" element
-  // document.getElementsByClassName("resultArea")[0].classList.remove("hidden")
+  document.getElementById('A1Z26Result').innerText = A1Z26Result.toUpperCase()
 }
